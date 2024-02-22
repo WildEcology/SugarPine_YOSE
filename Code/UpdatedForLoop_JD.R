4339061c11a1a08a3c90516d9aec61846c723ec2
Michelle D. Mohr <michelledmohr@ucsb.edu>
2024-02-22 11:17:44 -0800

    changed code because it was attaching NA to the file path before

--- Code/UpdatedForLoop_JD.R
+++ Code/UpdatedForLoop_JD.R
@@ -8,10 +8,10 @@ tree_list <- data.frame()
 for (folder in folders) {
   newfiles = list.files(folder, pattern = "PILAdata")
   for(file in newfiles) {
-    xlsfile <- read_excel(paste0(folder,"/",file, na = "NA")) %>%
+    xlsfile <- read_excel(paste0(folder,"/",file), na = "NA") %>%
       mutate_if(is.numeric, as.numeric) %>% 
       dplyr::select(plotID, treeNum,DBH_cm, pitchTubes, exitHoles, activeBranchCanker, inactiveBranchCanker, 
-                    activeBoleCanker, inactiveBoleCanker, notes, plot_elevation_m)
+                    activeBoleCanker, inactiveBoleCanker, notes, plot_elevation_ft)
     tree_list <- rbind(tree_list, xlsfile)
   }
 }
